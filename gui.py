7337f0c966747aab445a1b29d382a12e6f8a4b74
Reso de la Rescho <ahmed.bilgic@tu-dortmund.de>
2025-06-26 10:14:56 +0200

    basic's

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as window
+import tkinter.font as font
+
+
+def creat_gui():
+    root = window.Tk()
+    root.title("KeyPresser")
+    root.geometry("300x300")
+    custom_font = font.Font(family="Arial", size=15)
